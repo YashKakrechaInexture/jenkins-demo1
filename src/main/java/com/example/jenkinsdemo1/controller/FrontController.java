5828507e1a360748180ff9e0d16aa34eddfa7e14
YashKakrechaInexture <yashvk.inexture@gmail.com>
2022-12-15 15:12:58 +0530

    conflicts resolved

--- src/main/java/com/example/jenkinsdemo1/controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo1/controller/FrontController.java
@@ -1,7 +1,15 @@
 package com.example.jenkinsdemo1.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FrontController {
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "home page";
+    }
 }
