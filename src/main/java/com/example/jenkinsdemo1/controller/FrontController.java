90b617ceef0889806e3628893ada6e46d29d76dc
Yash Kakrecha <100900021+YashKakrechaInexture@users.noreply.github.com>
2022-12-20 15:09:49 +0530

    changes

--- src/main/java/com/example/jenkinsdemo1/controller/FrontController.java
+++ src/main/java/com/example/jenkinsdemo1/controller/FrontController.java
@@ -10,6 +10,6 @@ public class FrontController {
     @GetMapping("/home")
     @ResponseBody
     public String home() {
-        return "updated dhauksnnd home page";
+        return "updated home page";
     }
 }
