23b0ad4b7cf5c63ad0a86dab706583fbbe03f260
YashKakrechaInexture <yashvk.inexture@gmail.com>
2022-12-20 15:07:20 +0530

    change

--- src/main/java/com/example/jenkinsdemo1/JenkinsDemo1Application.java
+++ src/main/java/com/example/jenkinsdemo1/JenkinsDemo1Application.java
@@ -2,9 +2,16 @@ package com.example.jenkinsdemo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemo1Application {
+public class JenkinsDemo1Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(JenkinsDemo1Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemo1Application.class, args);
